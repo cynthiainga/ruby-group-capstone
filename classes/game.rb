7355465761ddb6b9bcb33d7d84ec4f3b90151c38
backtofayton <yucelolcay@gmail.com>
2022-05-24 00:04:16 +0300

    Fix linter

--- classes/game.rb
+++ classes/game.rb
@@ -7,11 +7,10 @@ class Game < Item
     @last_played_at = last_played_at
   end
 
-  
   def can_be_archived?
     super()
     @can_be_archived = true if calculate_years_from_last_played(@last_played_at) > 2
-    return @can_be_archived
+    @can_be_archived
   end
 
   private
@@ -20,12 +19,12 @@ class Game < Item
     # Difference in years, less one if you have not had a birthday this year.
     now = DateTime.now
     date_data = last_played.split('-')
-    last_played = DateTime.new(date_data[0].to_i,date_data[1].to_i,date_data[2].to_i)
+    last_played = DateTime.new(date_data[0].to_i, date_data[1].to_i, date_data[2].to_i)
     a = now.year - last_played.year
-    a = a - 1 if (
-        now.month >  last_played.month or 
+    a -= 1 if
+        now.month > last_played.month or
         (now.month == last_played.month and now.day > last_played.day)
-    )
+
     a
   end
 end
