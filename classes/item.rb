7e7dce47aeac4fce0f016654436f9cbdb5965e52
backtofayton <yucelolcay@gmail.com>
2022-05-24 00:01:47 +0300

    Add methods for class: Game

--- classes/item.rb
+++ classes/item.rb
@@ -18,7 +18,8 @@ class Item
   end
 
   def can_be_archived?
-    true
+    @can_be_archived = true
+    @can_be_archived
   end
 
   def move_to_archive
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,4 +8,8 @@ describe Game do
   it 'should initialize with multiplier == false' do
     expect(@instance.multiplayer).to eq(false)
   end
+
+  it 'can be archived if parent can be archived' do
+    expect(@instance.can_be_archived?).to be(true)
+  end
 end
