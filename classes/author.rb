89ddce4d39e2c6d8b03d0eb7b0b72ded7d254312
backtofayton <yucelolcay@gmail.com>
2022-05-23 23:02:07 +0300

    Add tests for class Author

--- classes/author.rb
+++ classes/author.rb
@@ -1,17 +1,20 @@
+require_relative './item'
+
 class Author 
-  attr_accessor :first_name, :last:name
+  attr_accessor :first_name, :last_name
   attr_reader :items
   @@id = 0
   
   def initialize(first_name: 0, last_name: 0)
     @id = @@id
-    @@id++
+    @@id += 1
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
-  def add_item(item:0)
+  def add_item(item: 0)
+    return p 'Please add an item of class Item' if item.instance_of?(Item) == false
     @items.push(item)
   end
 
