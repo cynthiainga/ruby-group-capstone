7f599cec8b47484ac7eb858f7cc01724f68a0805
backtofayton <yucelolcay@gmail.com>
2022-05-24 15:43:50 +0300

    Add app methods for games

--- app.rb
+++ app.rb
@@ -1,1 +1,46 @@
 # new app
+require './classes/game'
+require './classes/author'
+
+class App
+  def initialize()
+    @games = []; 
+    @authors = [];
+  end
+
+  def ask_user(string)
+    p "#{string}: "
+    gets.chomp
+  end
+
+  def list_all_games()
+    @games.map do |game|
+      "Id: #{game.id}, Label: #{game.label}, Multiplayer: #{game.multiplayer}, Last played: #{game.last_played_at}"
+    end
+  end
+
+  def list_all_authors()
+    @authors.map do |author|
+      "Id: #{author.id}, First name: #{author.first_name}, Last name: #{author.last_name}"
+    end
+  end
+
+  def add_game()
+    _label = ask_user("Enter game label")
+    _multiplayer = ask_user("Is it multiplayer? [Y/N]").downcase
+
+    if _multiplayer == 'y'
+      _multiplayer = true
+    elsif _multiplayer == 'n'
+      _multiplayer = false
+    end
+    
+    new_game = Game.new(label: _label, multiplayer: _multiplayer)
+    @games.push(new_game)
+
+  end
+end
+
+a = App.new()
+a.add_game()
+
--- classes/game.rb
+++ classes/game.rb
@@ -1,8 +1,10 @@
+require_relative './item'
+
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer: false, last_played_at: DateTime.now.to_s.slice(0, 10))
-    super()
+  def initialize(multiplayer: false, last_played_at: 0, **args)
+    super(**args)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
