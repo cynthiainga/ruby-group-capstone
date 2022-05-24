aadc2d511a01ec92cb3a199a78d8596b415e3a78
backtofayton <yucelolcay@gmail.com>
2022-05-24 15:57:55 +0300

    Fix test

--- classes/game.rb
+++ classes/game.rb
@@ -6,7 +6,7 @@ class Game < Item
   def initialize(multiplayer: false, last_played_at: 0, **args)
     super(**args)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
+    @last_played_at = last_played_at unless last_played_at == 0
   end
 
   def can_be_archived?
@@ -19,13 +19,17 @@ class Game < Item
   def calculate_years_from_last_played(last_played)
     # Difference in years, less one if you have not had a birthday this year.
     now = DateTime.now
-    date_data = last_played.split('-')
-    last_played = DateTime.new(date_data[0].to_i, date_data[1].to_i, date_data[2].to_i)
-    a = now.year - last_played.year
-    a -= 1 if
-        now.month > last_played.month or
-        (now.month == last_played.month and now.day > last_played.day)
+    date_data = last_played&.split('-')
+    if date_data
+      last_played = DateTime.new(date_data[0].to_i, date_data[1].to_i, date_data[2].to_i)
+      a = now.year - last_played.year
+      a -= 1 if
+          now.month > last_played.month or
+          (now.month == last_played.month and now.day > last_played.day)
 
-    a
+      a
+    else
+      0
+    end
   end
 end
