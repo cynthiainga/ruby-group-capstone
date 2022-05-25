e44e0654a105f9d3f867ce85571e540275ddda5f
cynthiainga <candyinga67@gmail.com>
2022-05-25 16:09:16 +0200

    Add the method to display_genre

---
+++ modules/display_genre.rb
@@ -0,0 +1,8 @@
+require_relative '../classes/genre'
+
+def display_genres(genres)
+  puts
+  puts 'Database is empty. Add a genre.' id genres.empty?
+  genres.each { |genre| puts "[Genre] Name: #{genre.name}" }
+  puts
+end
