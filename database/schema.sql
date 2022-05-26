ba2e830b4038349c3d89c1d519e7cd69890d8b9a
cynthiainga <candyinga67@gmail.com>
2022-05-26 11:53:39 +0200

    Add item table to schema.sql file

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,18 @@
+CREATE DATABASE catalog;
+
+CREATE TABLE item (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    publish_date DATE,
+    archived BOOLEAN,
+    PRIMARY KEY(id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id),
+);
+
 CREATE TABLE game (
     last_played_at DATE,
     multiplayer BOOLEAN,
